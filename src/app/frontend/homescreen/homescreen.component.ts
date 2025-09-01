889c3fd048dee3619fcb2f1597627c61c672f16f
Ch1charito <r.buschmann.work@gmail.com>
2025-09-01 16:47:34 +0200

    added login, header and homescreen component

---
+++ src/app/frontend/homescreen/homescreen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-homescreen',
+  imports: [HeaderComponent],
+  templateUrl: './homescreen.component.html',
+  styleUrl: './homescreen.component.scss'
+})
+export class HomescreenComponent {
+
+}
