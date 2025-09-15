2838fb5a697c594d4a3a75e9a4a6ce36237d4718
basti <bastianseibel1@gmail.com>
2025-09-15 13:41:49 +0200

    feat: :sparkles: added enviroment.ts for firebase setup

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAWlMekGgN3cjviVVMjuC1_SjVba2PrrYU",
+    authDomain: "timely-64884.firebaseapp.com",
+    projectId: "timely-64884",
+    storageBucket: "timely-64884.firebasestorage.app",
+    messagingSenderId: "331492796471",
+    appId: "1:331492796471:web:d23e2fc76a6b61506af7b2",
+    measurementId: "G-TETH6MYC8G"
+  }
+};
