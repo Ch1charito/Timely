0a3ad445bffdec54409fce87275a843b29a775a4
basti <bastianseibel1@gmail.com>
2025-09-18 15:16:21 +0200

    feat: :bug: update enviroment file

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAWlMekGgN3cjviVVMjuC1_SjVba2PrrYU",
-    authDomain: "timely-64884.firebaseapp.com",
-    projectId: "timely-64884",
-    storageBucket: "timely-64884.firebasestorage.app",
-    messagingSenderId: "331492796471",
-    appId: "1:331492796471:web:d23e2fc76a6b61506af7b2",
-    measurementId: "G-TETH6MYC8G"
-  }
+    apiKey: 'AIzaSyBj9L3yTMzzMqlSkQO5-oe1gnrXGCOW49Q',
+    authDomain: 'timely-43f85.firebaseapp.com',
+    projectId: 'timely-43f85',
+    storageBucket: 'timely-43f85.firebasestorage.app',
+    messagingSenderId: '527733722731',
+    appId: '1:527733722731:web:5cfb9fcb019e5fc7492b2e',
+    measurementId: 'G-4B77DKSH60',
+  },
 };
