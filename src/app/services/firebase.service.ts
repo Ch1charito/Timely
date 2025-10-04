6b96821ba3903ab9b892b6e0f7e946c70496574f
bastianseibel <bastianseibel1@gmail.com>
2025-10-04 15:02:28 +0200

    feat: :sparkles: add listeners for materials and tools with helper functions

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject, OnDestroy } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 import { Material } from '../interfaces/material.interface';
 import { Tool } from '../interfaces/tool.interface';
 
@@ -11,8 +11,59 @@ export class FirebaseService implements OnDestroy {
 
   materials: Material[] = [];
   tools: Tool[] = [];
+  unsubscribeMaterials: () => void;
+  unsubscribeTools: () => void;
 
-  constructor() {}
+  constructor() {
+    this.unsubscribeMaterials = onSnapshot(
+      collection(this.firestore, 'materials'),
+      (materialsSnapshot) => {
+        this.materials = [];
+        materialsSnapshot.forEach((element) => {
+          this.materials.push(
+            this.setMaterialObject(element.id, element.data())
+          );
+        });
+        this.materials.sort((a, b) => a.name.localeCompare(b.name));
+      }
+    );
 
-  ngOnDestroy() {}
+    this.unsubscribeTools = onSnapshot(
+      collection(this.firestore, 'tools'),
+      (toolsSnapshot) => {
+        this.tools = [];
+        toolsSnapshot.forEach((element) => {
+          this.tools.push(this.setToolObject(element.id, element.data()));
+        });
+      }
+    );
+  }
+
+  /* Setter for Material and Tool objects */
+
+  setMaterialObject(id: string, obj: any): Material {
+    return {
+      id: id,
+      name: obj.name,
+      description: obj.description,
+      quantity: obj.quantity,
+      unit: obj.unit,
+      category: obj.category,
+    };
+  }
+
+  setToolObject(id: string, obj: any): Tool {
+    return {
+      id: id,
+      name: obj.name,
+      description: obj.description,
+      category: obj.category,
+      inUse: obj.inUse,
+    };
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeMaterials();
+    this.unsubscribeTools();
+  }
 }
