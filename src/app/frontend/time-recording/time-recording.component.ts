3ed32d1af8183751a8044380c6cc284986fe81d6
Ch1charito <r.buschmann.work@gmail.com>
2025-10-06 12:05:15 +0200

    added header and back to homescreen button

--- src/app/frontend/time-recording/time-recording.component.ts
+++ src/app/frontend/time-recording/time-recording.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-time-recording',
-  imports: [],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './time-recording.component.html',
   styleUrl: './time-recording.component.scss'
 })
