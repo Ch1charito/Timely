24f3158a5e1a5ae9475082f7876edd0308f8b403
Ch1charito <r.buschmann.work@gmail.com>
2025-10-06 11:26:28 +0200

    added new routes for travelling between copmonents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './frontend/login/login.component';
 import { HomescreenComponent } from './frontend/homescreen/homescreen.component';
+import { TimeRecordingComponent } from './frontend/time-recording/time-recording.component';
+import { VacationManagementComponent } from './frontend/vacation-management/vacation-management.component';
+import { ConstructionMaterialsComponent } from './frontend/construction-materials/construction-materials.component';
+import { ConstructionToolsComponent } from './frontend/construction-tools/construction-tools.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     {path: 'login', component: LoginComponent},
     {path: 'homescreen', component: HomescreenComponent },
+    {path: 'time', component: TimeRecordingComponent},
+    {path: 'vacation', component: VacationManagementComponent},
+    {path: 'materials', component: ConstructionMaterialsComponent},
+    {path: 'tools', component: ConstructionToolsComponent},
 ];
