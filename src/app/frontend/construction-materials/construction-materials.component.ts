502c29ca6fc94af9f16c3c808916ee7b95900ce2
bastianseibel <bastianseibel1@gmail.com>
2025-10-07 12:14:03 +0200

    feat: :sparkles: import firebaseservice

--- src/app/frontend/construction-materials/construction-materials.component.ts
+++ src/app/frontend/construction-materials/construction-materials.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-construction-materials',
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, CommonModule],
   templateUrl: './construction-materials.component.html',
-  styleUrl: './construction-materials.component.scss'
+  styleUrl: './construction-materials.component.scss',
 })
 export class ConstructionMaterialsComponent {
-
+  constructor(public firebaseService: FirebaseService) {}
 }
