9eab04c03bd4d65613007e91f9f98a476445df52
bastianseibel <bastianseibel1@gmail.com>
2025-10-07 12:27:36 +0200

    feat: :fire: removed commonmodule, not needed

--- src/app/frontend/construction-materials/construction-materials.component.ts
+++ src/app/frontend/construction-materials/construction-materials.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-construction-materials',
-  imports: [HeaderComponent, RouterModule, CommonModule],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './construction-materials.component.html',
   styleUrl: './construction-materials.component.scss',
 })
