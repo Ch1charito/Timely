37a75c85d973f1553acf2633bc7352938a548ee3
Ch1charito <r.buschmann.work@gmail.com>
2025-10-08 11:54:31 +0200

    added function for worksessions and timer

--- src/app/frontend/time-recording/time-recording.component.ts
+++ src/app/frontend/time-recording/time-recording.component.ts
@@ -1,13 +1,67 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-time-recording',
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, CommonModule],
   templateUrl: './time-recording.component.html',
   styleUrl: './time-recording.component.scss'
 })
 export class TimeRecordingComponent {
+hours: number = 0;
+minutes: number = 0;
+seconds: number = 0;
+
+intervalId: any;  
+isRunning: boolean = false;
+workSessions: { date: string; worktime: string }[] = [];
+
+get formattedTime(): string {
+  const h = this.hours.toString().padStart(2, '0');
+  const m = this.minutes.toString().padStart(2, '0');
+  const s = this.seconds.toString().padStart(2, '0');
+  return `${h}:${m}:${s}`;
+}
+
+startTimer(): void {
+  if (this.isRunning) return;
+  this.isRunning = true;
+  this.intervalId = setInterval(() => {
+    this.seconds++;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.minutes++;
+    }
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.hours++;
+    }
+  }, 1000);
+}
+
+stopTimer(): void {
+  if (!this.isRunning) return;
+  clearInterval(this.intervalId);
+  this.isRunning = false;
+}
+
+endWork(): void {
+  if (this.isRunning) {
+    clearInterval(this.intervalId);
+    this.isRunning = false;
+  }
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString('de-DE');
+  const workSession = {
+    date: formattedDate,
+    worktime: this.formattedTime
+  };
+  this.workSessions.push(workSession);
+  this.hours = 0;
+  this.minutes = 0;
+  this.seconds = 0;
+}
 
 }
