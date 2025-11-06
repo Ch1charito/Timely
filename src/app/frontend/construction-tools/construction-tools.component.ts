ace3e81e96bb00b97834685ca0420dc633aa3bea
Ch1charito <r.buschmann.work@gmail.com>
2025-11-06 12:24:28 +0100

    added more stiling for the switch button

--- src/app/frontend/construction-tools/construction-tools.component.ts
+++ src/app/frontend/construction-tools/construction-tools.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from "../header/header.component";
 import { RouterModule } from '@angular/router';
 import { FirebaseService } from '../../services/firebase.service';
 import { Tool } from '../../interfaces/tool.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-construction-tools',
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, CommonModule],
   templateUrl: './construction-tools.component.html',
   styleUrl: './construction-tools.component.scss'
 })
