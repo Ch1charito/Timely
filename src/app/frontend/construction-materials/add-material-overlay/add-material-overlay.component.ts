fc4debadce28df6b6a34fbea1e4077b90027313d
Ch1charito <r.buschmann.work@gmail.com>
2025-11-26 18:01:51 +0100

    added form to create new material

--- src/app/frontend/construction-materials/add-material-overlay/add-material-overlay.component.ts
+++ src/app/frontend/construction-materials/add-material-overlay/add-material-overlay.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-material-overlay',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-material-overlay.component.html',
   styleUrl: './add-material-overlay.component.scss'
 })
