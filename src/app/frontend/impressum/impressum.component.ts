0dd2039718edadd8cdb8d498e314af4385cb3520
Ch1charito <r.buschmann.work@gmail.com>
2025-12-03 15:59:01 +0100

    added privacy policy and impressum

---
+++ src/app/frontend/impressum/impressum.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-impressum',
+  imports: [HeaderComponent],
+  templateUrl: './impressum.component.html',
+  styleUrl: './impressum.component.scss'
+})
+export class ImpressumComponent {
+
+}
